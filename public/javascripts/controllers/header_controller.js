45a5c29e10e24880908f4e2c3b86477146499396
Grant Schulte <schulte.grant@gmail.com>
2014-03-16 18:04:22 -0400

    Adds drawer user ctrl menu

--- public/javascripts/controllers/header_controller.js
+++ public/javascripts/controllers/header_controller.js
@@ -1,6 +1,16 @@
 function HeaderCtrl( $scope, $rootScope, $location, loginService ) {
 
+  var init = function () {
+    $scope.ctrlOpen = false;
+  };
+
   $scope.logout = function () {
     loginService.logout();
   };
+
+  $scope.toggleCtrl = function () {
+    $scope.ctrlOpen = !$scope.ctrlOpen;
+  };
+
+  init();
 }
